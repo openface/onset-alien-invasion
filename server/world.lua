ec00d09733df3e32f0f1888083d317e4b35f5537
Jason Hines <jason@greenhell.com>
2020-01-18 17:26:27 -0500

    gamemode will include its own world.json

---
+++ server/world.lua
@@ -0,0 +1,24 @@
+function World_LoadWorld()
+  if WorldLoaded then return end
+  if IsPackageStarted('sandbox') then return error('ERROR: Not loading world since sandbox package is enabled, this package should only be used when sandbox is disabled.') end
+  WorldLoaded = true
+
+  print('Server: Attempting to load world.')
+
+  local _table = File_LoadJSONTable("packages/"..GetPackageName().."/server/data/world.json")
+  for _,v in pairs(_table) do
+    if v['modelID'] ~= nil then
+      CreateObject(v['modelID'], v['x'], v['y'], v['z'], v['rx'], v['ry'], v['rz'], v['sx'], v['sy'], v['sz'])
+    else
+      local _AddYaw = DoorConfig[tonumber(doorID)]
+      if _AddYaw == nil then
+        _AddYaw = 90
+      end
+
+      CreateDoor(v['doorID'], v['x'], v['y'], v['z'], v['yaw'] + _AddYaw, true)
+    end
+  end
+
+  print('Server: Alien Invasion World loaded!')
+end
+AddEvent('OnPackageStart', World_LoadWorld)

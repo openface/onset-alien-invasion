5ea089648c44770e060da22537b227e94c065ebb
Jason Hines <jason@greenhell.com>
2020-02-09 17:57:24 -0500

    drop part on death

--- client/parts.lua
+++ client/parts.lua
@@ -1,6 +1,6 @@
 AddRemoteEvent('PartPickedup', function(pickup)
     SetSoundVolume(CreateSound("client/sounds/part_pickup.wav"), 1)
-    ShowMessage("You have found a computer part.  Use this part on the satellite computer!")
+    ShowMessage("You have found a computer part.  Use this on the satellite computer!")
 
     ShowSatelliteWaypoint()    
 end)

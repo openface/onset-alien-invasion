eb2bc09d24385055538a097b953dd88854641dc7
Jason Hines <jason@greenhell.com>
2020-02-15 14:21:25 -0500

    reducing safe zone a bit

--- server/aliens.lua
+++ server/aliens.lua
@@ -2,7 +2,7 @@ local AlienHealth = 999
 local AlienAttackRange = 5000
 local AlienAttackDamage = 50
 local SafeLocation = { x = -102037, y = 194299, z = 1400 }
-local SafeRange = 8000
+local SafeRange = 5000
 local AlienRetargetCooldown = {} -- aliens re-target on every weapon hit w/ cooldown period
 
 -- TODO remove

63ae4bfcf828bf985b2a2b3ce7b23024bcc8a390
Jason Hines <jason@greenhell.com>
2020-09-12 13:30:53 -0400

    wip

--- server/computer.lua
+++ server/computer.lua
@@ -1,13 +1,12 @@
 local PartsCollected = 0
 local PartsRequired = 5
----local Satellite3DText
 
 AddEvent("OnPackageStart", function()
     -- central computer
-    CreateText3D("Press [E] to Interact", 15, -106279.4140625, 193854.59375, 1399.1424560547 + 130, 0,0,0)
+    CreateText3D("Press [E] to Interact", 12, -106279.4140625, 193854.59375, 1399.1424560547 + 130, 0,0,0)
 
     -- satellite computer
-    CreateText3D("Press [E] to Interact", 15, -103004.5234375, 201067.09375, 2203.3188476563 + 130, 0, 0, 0)
+    CreateText3D("Press [E] to Interact", 12, -103004.5234375, 201067.09375, 2203.3188476563 + 130, 0, 0, 0)
 end)
 
 AddRemoteEvent("InteractSatelliteComputer", function(player)

97ad1a88a80a171533cb5d025fc1db2914d95b5b
Jason Hines <jason@greenhell.com>
2020-09-15 18:11:26 -0400

    WIP

---
+++ server/data/workbench.lua
@@ -0,0 +1,17 @@
+return {
+    ['armor'] = {
+        ['name'] = 'Kevlar Vest', 
+        ['modelid'] = 843, 
+        ['scrap_needed'] = 15 
+    },
+    ['flashlight'] = {
+        ['name'] = 'Flashlight', 
+        ['modelid'] = 1271, 
+        ['scrap_needed'] = 10 
+    },
+    ['medkit'] = {
+        ['name'] = 'Medical Kit', 
+        ['modelid'] = 796, 
+        ['scrap_needed'] = 5 
+    },
+}

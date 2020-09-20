1b26bf2052da8c6c7521b2de879bbf5d7b4cecbe
Jason Hines <jason@greenhell.com>
2020-09-20 18:29:46 -0400

    combining workbench data and inventory data

---
+++ server/data/items.lua
@@ -0,0 +1,27 @@
+return {
+    [1] = {
+        ['name'] = 'Kevlar Vest', 
+        ['modelid'] = 843, 
+        ['scrap_needed'] = 15 
+    },
+    [2] = {
+        ['name'] = 'Flashlight', 
+        ['modelid'] = 1271, 
+        ['scrap_needed'] = 10 
+    },
+    [3] = {
+        ['name'] = 'Medical Kit', 
+        ['modelid'] = 796, 
+        ['scrap_needed'] = 5 
+    },
+    [4] = {
+        ['name'] = 'Scrap',
+        ['modelid'] = 694,
+        ['scrap_needed'] = 1
+    },
+    [5] = {
+        ['name'] = 'Computer Part',
+        ['modelid'] = 1437,
+        ['scrap_needed'] = 20
+    }
+}

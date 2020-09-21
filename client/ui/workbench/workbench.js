44c80b7142531672a977a474bf63bbb7b618fa2e
Jason Hines <jason@greenhell.com>
2020-09-21 00:03:51 -0400

    xtemplate

--- client/ui/workbench/workbench.js
+++ client/ui/workbench/workbench.js
@@ -1,4 +1,5 @@
 Vue.component('build-button', {
+    template: '#build-button',
     props: {
         player_scrap: { type: Number },
         scrap_needed: { type: Number },
@@ -13,17 +14,6 @@ Vue.component('build-button', {
             return this.building_item !== false;
         }
     },
-    template: `
-        <div v-if="isBuilding">
-            <div class="meter"><span><span class="progress"></span></span></div>
-        </div>
-        <div v-else-if="player_scrap >= scrap_needed">
-            <button class="build" :disabled="isDisabled" @click="build">BUILD</button>
-        </div>
-        <div v-else>
-            <button class="need_scrap" disabled="true">NEED SCRAP</button>
-        </div>
-    `,
     methods: {
         build() {
             EventBus.$emit('building_item', this.item)

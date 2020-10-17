636ff4cc9e04ab3e4c639757e49aa03e6b5d084e
Jason Hines <jason@greenhell.com>
2020-10-17 16:54:48 -0400

    flashlight

---
+++ client/effects.lua
@@ -0,0 +1,43 @@
+
+local Pointlights = {}
+
+AddEvent("OnObjectStreamIn", function(object)
+    local pointlight = GetObjectPropertyValue(object, "pointlight")
+    if pointlight == true then
+        AttachPointlight(object)
+    end
+end)
+
+AddEvent("OnObjectStreamOut", function(object)
+    if GetObjectPropertyValue(object, "pointlight") ~= nil then
+        DetachPointlight(object)
+    end
+end)
+
+AddEvent("OnObjectNetworkUpdatePropertyValue", function(object, PropertyName, PropertyValue)
+    if PropertyName == "pointlight" then
+        if PropertyValue == true then
+            AttachPointlight(object)
+        else
+            DetachPointlight(object)
+        end
+    end
+end)
+
+function AttachPointlight(object)
+    AddPlayerChat("attaching pointlight")
+
+    local actor = GetObjectActor(object)
+    light = actor:AddComponent(USpotLightComponent.Class())
+    light:SetIntensity(10000 * 30)
+    light:SetLightColor(FLinearColor(255, 255, 255, 0), true)
+    light:SetRelativeRotation(FRotator(0.0, 270.0, 0.0))
+
+    Pointlights[object] = light
+end
+
+function DetachPointlight(object)
+    AddPlayerChat("detaching pointlight")
+    -- destroy the component
+    Pointlights[object]:Destroy()
+end

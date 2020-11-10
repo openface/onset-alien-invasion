913af796b71e15cfbc1bc2a5830f33785eb400cc
Jason Hines <jason@greenhell.com>
2020-11-10 12:18:45 -0500

    adding car part assets

---
+++ server/objects/resources/exhaust.lua
@@ -0,0 +1,9 @@
+RegisterObject("exhaust", {
+  name = "Car Exhaust",
+  type = 'resource',
+  modelid = 30009,
+  image = "car_parts/SM_Car_Exhaust.jpg",
+  max_carry = 1,
+  recipe = nil,
+  attachment = nil
+})

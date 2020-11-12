fc957ec7b640b8a592986f377dde05265f9a805c
Jason Hines <jason@greenhell.com>
2020-11-11 22:43:54 -0500

    repair vehicle; refactor

---
+++ server/objects/computer_part.lua
@@ -0,0 +1,9 @@
+RegisterObject("computer_part", {
+    name = "Computer Part",
+    type = 'resource',
+    modelid = 1437,
+    scale = { x = 2.5, y = 2.5, z = 2.5 },
+    max_carry = 10,
+    recipe = nil,
+    attachment = nil
+})
---
+++ server/objects/metal.lua
@@ -0,0 +1,13 @@
+RegisterObject("metal", {
+    name = "Metal",
+    type = 'resource',
+    modelid = 597,
+    scale = {
+        x = 0.2,
+        y = 0.2,
+        z = 0.2
+    },
+    max_carry = 25,
+    recipe = nil,
+    attachment = nil
+})

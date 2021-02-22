edaa530f41eee4abe84846d73251ebee431bb7eb
Jason Hines <jason@greenhell.com>
2021-02-22 16:13:19 -0500

    adding ak47 gold

---
+++ items/ak47g_s.lua
@@ -0,0 +1,9 @@
+ItemConfig["ak47g"] = {
+    name = "AK47 (Gold)",
+    type = 'weapon',
+    weapon_id = 13,
+    modelid = 15,
+    mag_item = "rifle_mag",
+    mag_size = 30,
+    auto_equip = true
+}

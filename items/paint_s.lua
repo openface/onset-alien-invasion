b4a009fe11ed324be3c7fae58d74e9f3f7af58ae
Jason Hines <jason@greenhell.com>
2021-03-23 13:14:24 -0400

    paint

---
+++ items/paint_s.lua
@@ -0,0 +1,33 @@
+ItemConfig["paint"] = {
+    name = "Bucket of Paint",
+    type = 'usable',
+    category = "Supplies",
+    modelid = 1576,
+    max_carry = 1,
+    max_use = 3,
+    interactions = {
+        vehicle_hood = {
+            use_label = "Paint Vehicle",
+            sound = "sounds/paint.mp3",
+            animation = { name = "BARCLEAN01", duration = 7000 },
+            event = "StartMechanic"
+        },
+        equip = {
+            event = "EquipPaint"
+        }
+    },
+    attachment = {
+        x = -31.7,
+        y = 32,
+        z = -5.1,
+        rx = 74.5,
+        ry = -180.4,
+        rz = 0,
+        bone = "hand_r"
+    },
+    price = 50
+}
+
+AddEvent("EquipPaint", function(player, object)
+    SetPlayerAnimation(player, "CARRY_IDLE")
+end)
